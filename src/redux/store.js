3306ca6d6752ff2c79057cff97abe519ccaba9d2
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-01-16 22:05:27 +0530

    some changes

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { cartreducer } from "./reducer";
-const store=configureStore({
-    reducer:{
-        cart:cartreducer.reducer,
-    }
-})
-export default store;
